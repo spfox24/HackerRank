ce7c84dfde1a7267d6f5745ad20e5d2d5012242f
spfox24 <68411909+spfox24@users.noreply.github.com>
2022-01-17 10:36:19 -0800

    updates challenge

--- daily-challenge.js
+++ daily-challenge.js
@@ -95,3 +95,25 @@ function getPerimeter(length, width) {
 getArea();
 getPerimeter();
 
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+
+const s = 'javascriptloops';
+
+function vowelsAndConsonants(s) {
+    const vowels = 'aeiou';
+    let consonants = '';
+
+    for(let i = 0; i < s.length; i++) {
+        if (vowels.includes(s[i])) {
+            console.log(s[i]);
+        } else {
+            consonants += s[i] + '\n';
+        }
+    }
+    console.log(consonants.trim());
+}
+
+vowelsAndConsonants();
