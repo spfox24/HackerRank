9f7e5ce30388dbca7c0b7361ea72680a049dd3e2
spfox24 <68411909+spfox24@users.noreply.github.com>
2022-01-17 10:08:22 -0800

    updates challenge

--- daily-challenge.js
+++ daily-challenge.js
@@ -57,3 +57,41 @@ function performOperation(secondInteger, secondDecimal, secondString) {
 
 performOperation();
 
+
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+
+const length = 3;
+const width = 4.5;
+
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    area = (length*width);
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter = (length+width)*2;
+    return perimeter;
+}
+
+getArea();
+getPerimeter();
+
