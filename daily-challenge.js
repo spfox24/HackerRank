df9d51eca88bf001346f02852f15a1ebb5c7c06c
spfox24 <68411909+spfox24@users.noreply.github.com>
2022-01-18 13:25:58 -0800

    updates challenge

--- daily-challenge.js
+++ daily-challenge.js
@@ -130,3 +130,18 @@ function factorial(n) {
 };
 
 factorial();
+
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    let r = 2.6;
+
+    const PI = Math.PI;
+    const area = (PI * Math.pow(r, 2));
+    const perimeter = (2 * PI * r);
+
+    // Print the area of the circle:
+    console.log(area);
+    // Print the perimeter of the circle:
+    console.log(perimeter);
+};
