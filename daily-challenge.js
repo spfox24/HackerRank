470d26681e23e9c54010e69b150ec20d161b372c
spfox24 <68411909+spfox24@users.noreply.github.com>
2022-01-19 11:14:03 -0800

    updates challenge

--- daily-challenge.js
+++ daily-challenge.js
@@ -150,3 +150,22 @@ function getGrade(score) {
 
     return grade;
 }
+
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    switch(true) {
+        case 'aeiou'.includes(s[0]):
+            letter = 'A';
+            break;
+        case 'bcdfg'.includes(s[0]):
+            letter = 'B';
+            break;
+        case 'hjklm'.includes(s[0]):
+            letter = 'C';
+            break;
+        default:
+            letter = 'D';
+    }
+    return letter;
+}
