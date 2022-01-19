ce2d41e907b06c1258229c107473d7689779cd75
spfox24 <68411909+spfox24@users.noreply.github.com>
2022-01-19 11:54:21 -0800

    updates challenge

--- daily-challenge.js
+++ daily-challenge.js
@@ -169,3 +169,26 @@ function getLetter(s) {
     }
     return letter;
 }
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+
+function getSecondLargest(nums) {
+    // Complete the function
+    let largest = nums[0];
+    let secondLargest = nums[0];
+
+    for (let i = 0; i < nums.length; i++) {
+        if(nums[i] > largest) {
+            secondLargest = largest;
+            largest = nums[i];
+        }
+        if ((nums[i] > secondLargest) && (nums[i] < largest)) {
+            secondLargest = nums[i];
+        }
+    }
+    return secondLargest;
+}
