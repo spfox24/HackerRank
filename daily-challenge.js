cc0e4a75a7256417d17b742ae07ebc215ee93255
spfox24 <68411909+spfox24@users.noreply.github.com>
2022-01-24 14:25:26 -0800

    updates challenge

--- daily-challenge.js
+++ daily-challenge.js
@@ -192,3 +192,17 @@ function getSecondLargest(nums) {
     }
     return secondLargest;
 }
+
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+    try {
+        s = s.split('').reverse().join('');
+    } catch(error) {
+        console.log(error.message);
+    } finally {
+        console.log(s);
+    };
+};
