5207b7a6e6d480a52e5c0229c73be8a75018de2f
spfox24 <68411909+spfox24@users.noreply.github.com>
2022-02-02 16:15:29 -0800

    updates challenges

--- daily-challenge.js
+++ daily-challenge.js
@@ -232,7 +232,19 @@ function Rectangle(a, b) {
     this.area = a * b;
 }
 
-
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    let n = 0;
+    for(let o of objects) {
+        n += (o.x == o.y);
+    };
+    return n;
+};
 
 
 
